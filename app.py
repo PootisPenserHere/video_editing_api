c4eee4f6946cef2ca461586ab25f71cc359d7fb2
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-03-31 10:13:04 -0700

    [add] Clip creation by endpoint

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def hello():
 
 @app.route('/cut/<fileName>/starting/<startTime>/ending/<endTime>')
 def newCutVideo(fileName, startTime, endTime):
-    return getFileExtension(fileName)
-    #return cutVideo(fileName, startTime, endTime):
+    #return getFileExtension(fileName)
+    return cutVideo(fileName, startTime, endTime)
 
 def randomString(size = 16, chars = string.ascii_uppercase + string.digits):
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(size))
@@ -27,23 +27,23 @@ def getFileExtension(fileName):
 
 def cutVideo(fileName, startTime, endTime):
     fileExtension = getFileExtension(fileName)
-    originalFile = "%s%s%s" % ("videos/", fileName, fileExtension)
+    originalFile = "%s%s" % ("videos/", fileName)
 
-    randomString = randomString()
-    newFileName = "%s%s" % (randomString, fileExtension)
+    randomName = randomString()
+    newFileName = "%s%s" % (randomName, fileExtension)
     newFilePath = "%s%s" % ("videos/", newFileName)
 
     # Load video and select the subclip
-    #clip = VideoFileClip(originalFile).subclip(startTime, endTime)
+    clip = VideoFileClip(originalFile).subclip(float(startTime),float(endTime))
 
     # Reduce the audio volume (volume x 0.8)
-    #clip = clip.volumex(0.8)
+    clip = clip.volumex(0.8)
 
     # Overlay the text clip on the first video clip
-    #video = CompositeVideoClip([clip])
+    video = CompositeVideoClip([clip])
 
     # Write the result to a file (many options available !)
-    #video.write_videofile(newFilePath)
+    video.write_videofile(newFilePath)
 
     return newFileName
 
