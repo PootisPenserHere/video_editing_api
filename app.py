1f4abecb8d6034959cf250a5b787b08fa3e25ec7
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-03-31 21:34:57 -0700

    [add] Basis file upload

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ import imageio
 imageio.plugins.ffmpeg.download()
 from moviepy.editor import *
 from flask import send_from_directory
+from flask import request
+from flask import jsonify
+from flask import render_template
 import string
 import random
 import os.path
+from werkzeug import secure_filename
 
 app = Flask(__name__)
+
+# Api config
+app.config['UPLOAD_FOLDER'] = "videos/"
+app.config['ALLOWED_EXTENSIONS '] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'mp4'])
 
 @app.route('/')
 def hello():
@@ -47,9 +55,24 @@ def cutVideo(fileName, startTime, endTime):
 
     return newFileName
 
+def allowedExtensions(filename):
+    return '.' in filename and \
+          filename.rsplit('.', 1)[1].lower() in app.config['UPLOAD_FOLDER']
+
 @app.route('/retrieve/<fileName>')
 def retrieveVideo(fileName):
     return send_from_directory(directory ='/code/videos/', filename = fileName)
+
+@app.route('/upload')
+def formUploadFile():
+   return render_template('upload.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploadFile():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
