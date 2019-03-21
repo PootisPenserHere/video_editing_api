6bbdf2b7db2fb59564848c98a6b8be8eb8a841fc
Jose Pablo Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2019-03-21 10:42:12 -0700

    [mod] Code style following pep 8
    Docblocks have been corrected to follow pep 257

--- app.py
+++ app.py
@@ -10,73 +10,100 @@ import os.path
 
 app = Flask(__name__)
 
-# Api config
 app.config['UPLOAD_FOLDER'] = "videos/"
 app.config['ALLOWED_EXTENSIONS'] = set(['.flv', '.gif', '.gifv', '.avi', '.mpg', '.mp4', '.3gp'])
+
 
 @app.route('/cut/<fileName>/starting/<startTime>/ending/<endTime>')
 def newCutVideo(fileName, startTime, endTime):
     return cutVideo(fileName, startTime, endTime)
 
+
 @app.route('/volume/<fileName>/<newLevel>')
 def newVideoLowerVolume(fileName, newLevel):
     return reduceVolume(fileName, newLevel)
 
+
 @app.route('/retrieve/<fileName>')
 def retrieveVideo(fileName):
-    return send_from_directory(directory ='/code/videos/', filename = fileName)
+    return send_from_directory(directory='/code/videos/', filename=fileName)
 
-''' Renders a basic html template to allow the upload of video files from the
-browser rather than having to do it through a requesting application
-'''
+
 @app.route('/upload')
 def formUploadFile():
-   return render_template('upload.html')
+    """
+    Renders a basic html template to allow the upload of video files from the
+    browser rather than having to do it through a requesting application
+    """
 
-''' Stores the sent files through a post method, validating them against the
-permited formats and assigning them a randomly generated name
+    return render_template('upload.html')
 
-@return json - contains a status and the name of the new file if successful
-'''
-@app.route('/uploader', methods = ['POST'])
+
+@app.route('/uploader', methods=['POST'])
 def uploadFile():
-  receivedFile = request.files['file']
-  receivedFileFormat = uploadedFileExtension(receivedFile.filename)
+    """
+    Stores the sent files through a post method, validating them against the
+    permited formats and assigning them a randomly generated name
 
-  if receivedFileFormat in app.config['ALLOWED_EXTENSIONS']:
-      randomName = randomString()
-      newFileName = "%s%s" % (randomName, receivedFileFormat)
-      newFilePath = "%s%s" % (app.config['UPLOAD_FOLDER'], newFileName)
+    :return: contains a status and the name of the new file if successful
+    :rtyoe: json
+    """
 
-      receivedFile.save(newFilePath)
+    receivedFile = request.files['file']
+    receivedFileFormat = uploadedFileExtension(receivedFile.filename)
 
-      return jsonify({"status": "success", "message": newFileName})
+    if receivedFileFormat in app.config['ALLOWED_EXTENSIONS']:
+        randomName = randomString()
+        newFileName = "%s%s" % (randomName, receivedFileFormat)
+        newFilePath = "%s%s" % (app.config['UPLOAD_FOLDER'], newFileName)
 
-  else:
-      return jsonify({"status": "error", "message": "Format not allowed"})
+        receivedFile.save(newFilePath)
 
-''' Generates a cryptographically secure random string with a default lenght of
-20 characters
-'''
-def randomString(size = 20, chars = string.ascii_uppercase + string.digits):
+        return jsonify({"status": "success", "message": newFileName})
+
+    else:
+        return jsonify({"status": "error", "message": "Format not allowed"})
+
+
+def randomString(size: str =20):
+    """
+    Generates a cryptographically secure random string with a default length
+    of 20 characters
+
+    :param size: Desired size for the string to generate
+    :type size: int
+    :rtype: str
+    :return:
+    """
+
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(size))
 
-''' Obtains the extension of a file which already exists in disk
 
-@param filename string - the name of the file
-@return string
-'''
-def getFileExtension(fileName):
+def getFileExtension(fileName: str) -> str:
+    """
+    Obtains the extension of a file which already exists in disk
+
+    :param fileName: Name of the file with its extension included
+    :type fileName: str
+    :return:
+    """
+
     return os.path.splitext(fileName)[1]
 
-''' Creates a new clip of the original video within the selected timeframe
 
-@param filename string - the name of the file
-@param startTime int - the second from which the new clip will begin
-@param endTime int - the second on which the new clip will end
-@return json - contains a status and the name of the new file if successful
-'''
-def cutVideo(fileName, startTime, endTime):
+def cutVideo(fileName: str, startTime: int, endTime: int):
+    """
+    Creates a new clip of the original video within the selected timeframe
+
+    :param fileName: Name of the file with its extension
+    :type fileName: str
+    :param startTime: The second of the original video from which the new clip will begin
+    :type startTime: int
+    :param endTime: The second from the original video up to which the clip will be cut
+    :type endTime: int
+    :return:
+    """
+
     fileExtension = getFileExtension(fileName)
     originalFile = "%s%s" % (app.config['UPLOAD_FOLDER'], fileName)
 
@@ -85,21 +112,26 @@ def cutVideo(fileName, startTime, endTime):
     newFilePath = "%s%s" % (app.config['UPLOAD_FOLDER'], newFileName)
 
     # Load video and select the subclip
-    clip = VideoFileClip(originalFile).subclip(float(startTime),float(endTime))
+    clip = VideoFileClip(originalFile).subclip(float(startTime), float(endTime))
 
     # Write the result to a file
     clip.write_videofile(newFilePath)
 
     return jsonify({"status": "success", "message": newFileName})
 
-''' Chances the volume setting of a video, this process can only lower it from
-the already existing point
 
-@param filename string - the name of the file
-@param newVolume int - the new setting where 10 represents the 100%
-@return json - contains a status and the name of the new file if successful
-'''
-def reduceVolume(fileName, newVolume):
+def reduceVolume(fileName: str, newVolume: int):
+    """
+    Chances the volume setting of a video, this process can only lower it
+    from the already existing point
+
+    :param fileName: Name of the file with its extension
+    :type fileName: str
+    :param newVolume: The new setting where 10 represents the 100%
+    :type newVolume: int
+    :return:
+    """
+
     newVolume = float(newVolume)
     newVolume = newVolume / 10
 
@@ -121,14 +153,20 @@ def reduceVolume(fileName, newVolume):
 
     return jsonify({"status": "success", "message": newFileName})
 
-''' Returns the extension from an uploaded file
 
-@param filename string - the name of the file
-@return string
-'''
-def uploadedFileExtension(filename):
+def uploadedFileExtension(filename: str) -> str:
+    """
+    Returns the extension from a file name
+
+    :param filename: Name of the file with its extension
+    :type filename: str
+    :return: The extension of the file
+    :rtype: str
+    """
+
     extension = filename.rsplit('.', 1)[1].lower()
     return "%s%s" % (".", extension)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
